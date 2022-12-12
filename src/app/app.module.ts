bc14ea1210956999bb4d7c385671b82c11bbaf3a
Mike Pattyn <mike.pattyn1@gmail.com>
2022-12-12 07:38:50 +0100

    trololol

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,12 @@ import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SigninComponent } from './signin/signin.component';
 import { CarouselComponent } from './carousel/carousel.component';
-import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SigninComponent,
     CarouselComponent,
-    SignupComponent,
   ],
   imports: [
     BrowserModule,
