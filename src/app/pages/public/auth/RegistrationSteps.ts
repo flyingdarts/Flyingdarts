f79fb75e735d936fad159ededdf214b7cc348d49
Mike Pattyn <mike.pattyn1@gmail.com>
2023-05-04 20:15:58 +0200

    Update registration steps

---
+++ src/app/pages/public/auth/RegistrationSteps.ts
@@ -0,0 +1,8 @@
+
+
+export enum RegistrationSteps {
+  CreateAccount = 0,
+  CreateProfile = 1,
+  GiveCameraPermission = 2,
+  Completed = 9999
+}
--- src/app/pages/public/auth/auth.component.ts
+++ src/app/pages/public/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticatorService } from '@aws-amplify/ui-angular';
+import { RegistrationSteps } from './RegistrationSteps';
 
 @Component({
   selector: 'app-auth',
@@ -8,14 +9,119 @@ import { AuthenticatorService } from '@aws-amplify/ui-angular';
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent implements OnInit {
-
-  constructor(public authenticator: AuthenticatorService, private router: Router) { 
+  public activeStep: number = RegistrationSteps.CreateAccount;
+  constructor(public authenticator: AuthenticatorService, private router: Router) {
 
   }
 
   ngOnInit(): void {
     if (!!this.authenticator.user) {
-      this.router.navigate(['/'])
+      if (this.activeStep == RegistrationSteps.CreateAccount) {
+        this.activeStep = RegistrationSteps.CreateProfile;
+      }
+      if (this.activeStep == RegistrationSteps.CreateProfile) {
+        this.activeStep = RegistrationSteps.GiveCameraPermission;
+      }
+      if (this.activeStep == RegistrationSteps.GiveCameraPermission) {
+        // todo: store the registration details
+        this.router.navigate(['/']);
+      }
+    } else {
+      this.router.navigate(['/auth']);
     }
   }
+
+  async accessCamera() {
+    const videoPlayer = document.querySelector('video') as HTMLVideoElement;
+    await this.requestCameraPermissions().then(stream => {
+      videoPlayer.srcObject = stream;
+      this.activeStep == RegistrationSteps.Completed;
+      this.populateCameraSelectList();
+    }).catch(error => {
+      console.error('Failed to attach stream to video element', error);
+    });
+  }
+  populateCameraSelectList() {
+    if (!this.checkCameraPermission()) {
+      return;
+    }
+    // Get the select element and video element from the DOM
+    const selectElement = document.querySelector<HTMLSelectElement>('#cameraFormSelectCamera');
+    const videoElement = document.querySelector<HTMLVideoElement>('#videoPlayer');
+
+    // Populate the select element with the available webcams
+    this.populateWebcamSelectList(selectElement!).then(devices => {
+      // Add an event listener to the select element to switch the webcam when the user selects a new option
+      selectElement!.addEventListener('change', () => {
+        const selectedDeviceId = selectElement!.value;
+        this.switchWebcam(selectedDeviceId, videoElement!);
+      });
+    });
+  }
+  async checkCameraPermission(): Promise<boolean> {
+    if (!navigator.permissions || !navigator.permissions.query) {
+      // Permissions API not supported, assume permission was granted
+      return true;
+    }
+    const permissionStatus = await navigator.permissions.query({ name: 'camera' as PermissionName });
+    return permissionStatus.state === 'granted';
+  }
+
+
+  async requestCameraPermissions(): Promise<MediaStream> {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      console.log('Camera access granted');
+      return stream;
+    } catch (error) {
+      console.error('Camera access denied', error);
+      throw error;
+    }
+  }
+  // Get the available webcams and populate a select element with their names and IDs
+  async populateWebcamSelectList(selectElement: HTMLSelectElement): Promise<MediaDeviceInfo[]> {
+    // Get the list of available media devices
+    const devices = await navigator.mediaDevices.enumerateDevices();
+
+    // Filter the list to only include video input devices
+    const videoDevices = devices.filter(device => device.kind === 'videoinput');
+
+    // Create an option element for each video device and add it to the select element
+    videoDevices.forEach(device => {
+      const option = document.createElement('option');
+      option.value = device.deviceId;
+      option.text = device.label || `Camera ${selectElement.options.length + 1}`;
+      selectElement.appendChild(option);
+    });
+    console.log(videoDevices);
+    // Return the list of video devices
+    return videoDevices;
+  }
+
+  // Switch the video stream to the selected webcam
+  async switchWebcam(selectedDeviceId: string, videoElement: HTMLVideoElement): Promise<void> {
+    // Get the current video stream
+    const stream = videoElement.srcObject as MediaStream;
+
+    // Stop the current video stream
+    if (stream) {
+      stream.getTracks().forEach(track => track.stop());
+    }
+
+    // Get the media constraints for the selected device
+    const constraints: MediaStreamConstraints = {
+      video: {
+        deviceId: { exact: selectedDeviceId }
+      }
+    };
+
+    // Get the new video stream
+    const newStream = await navigator.mediaDevices.getUserMedia(constraints);
+
+    // Set the new video stream as the source of the video element
+    videoElement.srcObject = newStream;
+  }
+
 }
+
+
