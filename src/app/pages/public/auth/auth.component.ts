de6692646df4592b2249993ae9ed19fe20746b53
Mike Pattyn <mike.pattyn1@gmail.com>
2023-05-04 16:31:25 +0200

    Update components and authentication

--- src/app/pages/public/auth/auth.component.ts
+++ src/app/pages/public/auth/auth.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticatorService } from '@aws-amplify/ui-angular';
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authenticator: AuthenticatorService, private router: Router) { 
 
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+    if (!!this.authenticator.user) {
+      this.router.navigate(['/'])
+    }
+  }
 }
