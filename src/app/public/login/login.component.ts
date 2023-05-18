803dd4d4c58638cc4d15e7065868a36842c622e0
Mike Pattyn <mike.pattyn1@gmail.com>
2023-05-18 10:57:41 +0200

    Fix login

--- src/app/public/login/login.component.ts
+++ src/app/public/login/login.component.ts
@@ -25,7 +25,7 @@ export class LoginComponent implements OnInit {
         }
       }
     }
-     else if (!this.stateService.currentOnboardingState.profileCompleted && this.stateService.currentOnboardingState.facebookId != null) {
+     else if (!this.stateService.currentOnboardingState.profileCompleted && this.stateService.currentOnboardingState.facebookId == null) {
       console.log("routing to profile");
       this.router.navigate(['/', 'onboarding', { outlets: { 'onboarding-outlet': ['profile'] } }])
     } else if (this.stateService.currentOnboardingState.profileCompleted && !this.stateService.currentOnboardingState.cameraPermissionsGranted && this.stateService.currentOnboardingState.facebookId != null) {
