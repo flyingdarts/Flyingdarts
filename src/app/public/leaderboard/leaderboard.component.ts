c9fd08629f6334ae368991a9396c42c07dd7232f
Mike Pattyn <mike.pattyn1@gmail.com>
2023-05-20 12:15:04 +0200

    tempate update this

--- src/app/public/leaderboard/leaderboard.component.ts
+++ src/app/public/leaderboard/leaderboard.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AnimationOptions } from 'ngx-lottie';
+import { Subscription } from 'rxjs';
+import { AmplifyAuthService } from 'src/app/services/amplify-auth.service';
 
 @Component({
   selector: 'app-leaderboard',
   templateUrl: './leaderboard.component.html',
   styleUrls: ['./leaderboard.component.scss']
 })
-export class LeaderboardComponent implements OnInit {
+export class LeaderboardComponent implements OnInit, OnDestroy {
+  public isAuthenticated: boolean = false; // Initial value is false
+  private isAuthenticatedSubscription?: Subscription = undefined;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(public authService: AmplifyAuthService) { }
+  ngOnDestroy(): void {
+    this.isAuthenticatedSubscription?.unsubscribe();
   }
 
+  ngOnInit(): void {
+    this.isAuthenticatedSubscription = this.authService.isAuthenticated$.subscribe(
+      (isAuthenticated: boolean) => {
+        this.isAuthenticated = isAuthenticated;
+      }
+    );
+  }
+  public lottieOptions: AnimationOptions = {
+    path: '/assets/animations/flyingdarts_header.json',
+    loop: false
+  };
 }
