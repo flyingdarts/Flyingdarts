57c06c580df4831a0f06acb546bd476423777304
Mike Pattyn <mike.pattyn1@gmail.com>
2023-05-27 08:14:11 +0200

    Update package

--- src/app/resolvers/profile.resolver.ts
+++ src/app/resolvers/profile.resolver.ts
@@ -8,6 +8,8 @@ import { Observable, of } from 'rxjs';
 import { WebSocketService } from '../infrastructure/websocket/websocket.service';
 import { AppStore } from '../app.store';
 import { WebSocketActions } from '../infrastructure/websocket/websocket.actions.enum';
+import { UserProfileDetails } from '../shared/models/user-profile-details.model';
+import { isNullOrUndefined } from '../app.component';
 
 @Injectable({
   providedIn: 'root'
@@ -17,8 +19,12 @@ export class ProfileResolver implements Resolve<boolean> {
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
     return new Observable<boolean>((observer) => {
       const profileSubscription = this.webSocketService.getMessages().subscribe(x=> {
-        if (x.action === WebSocketActions.UserProfileGet) {
-          observer.next(true);
+        if (x.action === WebSocketActions.UserProfileGet && isNullOrUndefined(x.message as UserProfileDetails)) {
+            observer.next(true);
+            observer.complete();
+        } else {
+          this.router.navigate(['/', 'lobby'])
+          observer.next(false);
           observer.complete();
         }
       });
