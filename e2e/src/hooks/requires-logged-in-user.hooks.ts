79a29220db8831caf5dc3d686b28778d84d50e72
Mike Pattyn <mike@methylium.com>
2023-09-20 17:27:00 +0200

    Small update

--- e2e/src/hooks/requires-logged-in-user.hooks.ts
+++ e2e/src/hooks/requires-logged-in-user.hooks.ts
@@ -3,21 +3,21 @@ import { browser } from "@wdio/globals";
 import PropertiesReader from "properties-reader";
 
 Before({ tags: "@requires-logged-in-user", timeout: 10 * 1000 }, async () => {
-    await browser.setTimeout({implicit: 5000, pageLoad: 10000});
+    await browser.setTimeout({ implicit: 5000, pageLoad: 10000 });
     await browser.setWindowSize(1920, 1080);
     await browser.url("http://localhost:4200");
 
     const projectPropsPath: string = 'e2e/local.properties'
 
-    const pool = String(PropertiesReader(projectPropsPath).get('pool'));
-    const user = String(PropertiesReader(projectPropsPath).get("lastAuthUser"));
-    const signin = String(PropertiesReader(projectPropsPath).get("amplify-signin-with-hostedUI"))
-    const redirect = String(PropertiesReader(projectPropsPath).get("amplify-redirected-from-hosted-ui"))
-    const accessToken = String(PropertiesReader(projectPropsPath).get("accessToken"))
-    const idToken = String(PropertiesReader(projectPropsPath).get("idToken"))
-    const clockDrift = String(PropertiesReader(projectPropsPath).get("clockDrift"))
-    const refreshToken = String(PropertiesReader(projectPropsPath).get("refreshToken"))
-    const jsonString = String(PropertiesReader(projectPropsPath).get("userData"));
+    const pool = process.env.POOL ?? String(PropertiesReader(projectPropsPath).get('pool'));
+    const user = process.env.USER ?? String(PropertiesReader(projectPropsPath).get("lastAuthUser"));
+    const signin = process.env.SIGNIN ?? String(PropertiesReader(projectPropsPath).get("amplify-signin-with-hostedUI"))
+    const redirect = process.env.REDIRECT ?? String(PropertiesReader(projectPropsPath).get("amplify-redirected-from-hosted-ui"))
+    const accessToken = process.env.ACCESS_TOKEN ?? String(PropertiesReader(projectPropsPath).get("accessToken"))
+    const idToken = process.env.ID_TOKEN ?? String(PropertiesReader(projectPropsPath).get("idToken"))
+    const clockDrift = process.env.CLOCK_DRIFT ?? String(PropertiesReader(projectPropsPath).get("clockDrift"))
+    const refreshToken = process.env.REFRESH_TOKEN ?? String(PropertiesReader(projectPropsPath).get("refreshToken"))
+    const jsonString = process.env.USER_DATA ?? String(PropertiesReader(projectPropsPath).get("userData"));
     const userData = JSON.parse(JSON.parse(jsonString))
 
     await browser.executeScript(`localStorage.setItem("amplify-signin-with-hostedUI", "${signin}");`, [])
--- src/app/account/settings/settings.component.ts
+++ src/app/account/settings/settings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+  public lottieOptions: AnimationOptions = {
+    path: '/assets/animations/flyingdarts_icon.json',
+    loop: true
+  };
+
+  public settingsForm: FormGroup = new FormGroup({});
+  public isLoading: boolean = false;
+  public loadingTitle: string = "loading";
+  public loadingSubtitle: string = "is so slooow";
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  public updateSettings() {
 
+  }
 }
